bb6584ef3d1fb54c51ba779d43c29fb37387b609
rakibulislamrohan <rakibulislamrohanm@gmail.com>
2023-10-31 19:24:45 +0600

    onliy create for navBar

---
+++ src/component/Nevigation-bar/Nevigation-bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NevBar = (props) => {
+    return (
+        
+        <div>
+            <h1>This is another header</h1>
+        </div>
+    );
+};
+
+export default NevBar;
