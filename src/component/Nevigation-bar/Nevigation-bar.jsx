a8fe7470469c2c63f3f52a41fab5dec8ab9ee5c9
rakibulislamrohan <rakibulislamrohanm@gmail.com>
2023-10-31 21:51:28 +0600

    nevigationBar is complete

--- src/component/Nevigation-bar/Nevigation-bar.jsx
+++ src/component/Nevigation-bar/Nevigation-bar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import logo from '../../images/Logo.svg';
+import './nevigation-bar.css';
 const NevBar = (props) => {
     return (
         
-        <div>
-            <h1>This is another header</h1>
+        <div className='Navbar'>
+
+            <div className='Logo' >
+
+                <img src={logo}/>
+
+            </div>
+            <div className='button'>
+                <a className='order' href="">Order</a>
+                <a className='order' href="">Order Review</a>
+                <a className='order' href=""> Manage Inventory</a>
+                <a className='order' href=""> login</a>
+
+            </div>
+
         </div>
     );
 };
