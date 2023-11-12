8e6adb2e9ce9b22cd2511b1f8554f879838adf57
rakibulislamrohan <rakibulislamrohanm@gmail.com>
2023-11-12 21:10:32 +0600

    add to car button in completed

--- src/component/product/Product.jsx
+++ src/component/product/Product.jsx
@@ -3,20 +3,20 @@ import './product.css';
 
 
 const Product = (props) => {
-    const { img, name, seller, ratings, price } = props.product 
+    const { img, name, seller, ratings, price, } = props.product 
     return (
         
         <div className='product'>
                  <img src={img} alt="" />
 
-                <div className='product-information'>
+             <div className='product-information'>
 
-                 <h6 className='product-name'>{name}</h6>
-                 <p className='price'>price:{price}</p>
-                 <p>Manufacturar:{seller}</p>
-                 <p>Ratings: {ratings}star</p>
-
-                </div>
+                  <h6 className='product-name'>{name}</h6>
+                  <p className='price'>price:${price}</p>
+                  <p className='seller'>Manufacturar:{seller}</p>
+                  <p className='rating'>Ratings: {ratings}star</p>
+            </div>
+            <button className='add-cart'>Add to Cart</button>
         </div>
     );
 };
